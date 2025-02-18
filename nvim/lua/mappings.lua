fc3a87cb3f439d725b7b6766a571bd0adb12ecd5
HubertasVin <hubertas.personal@proton.me>
2025-02-18 22:47:56 +0200

    FIX: Fixed minor issues with zshrc and nvim config, where prettier did not work. Added mapping descriptions for custom nvim binds.

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,23 +1,25 @@
-require("nvchad.mappings")
+require "nvchad.mappings"
 
 -- add yours here
 
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
-map("n", "K", vim.lsp.buf.hover, opts)
-map("n", "gd", vim.lsp.buf.definition, opts)
-map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+-- LSP related mappings with descriptions
+map("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover", table.unpack(opts) })
+map("n", "gd", vim.lsp.buf.definition, { desc = "LSP Go to Definition", table.unpack(opts) })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action", table.unpack(opts) })
 
 -- Open CMD line without holding shift
-map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", ";", ":")
 
--- Custom mapping: Map <leader>fj to HopWord in normal mode
-map("n", "<leader>fj", "<cmd>HopWord<CR>", { desc = "Hop Hop to word" })
+-- Custom mapping: Hop to word
+map("n", "<leader>fj", "<cmd>HopWord<CR>", { desc = "Hop to word", table.unpack(opts) })
 
 -- Paste without affecting clipboard
-map("v", "P", "p", { desc = "Clipboard Paste by copying selected text" })
-map("v", "p", "P", { desc = "Clipboard Paste without affecting clipboard" })
+map("v", "P", "p", { desc = "Paste (clipboard unaware)", table.unpack(opts) })
+map("v", "p", "P", { desc = "Alternate paste (clipboard preserved)", table.unpack(opts) })
 
 -- Remap '>' and '<' to keep selection in visual mode
-map("v", "<", "<gv", { noremap = true, silent = true })
-map("v", ">", ">gv", { noremap = true, silent = true })
+map("v", "<", "<gv")
+map("v", ">", ">gv")
